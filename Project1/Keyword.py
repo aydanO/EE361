681d4a7eb0fd2fa6f5c6a2da5f0417f0310074ef
aydano <obrienaydan630@gmail.com>
2023-03-10 11:38:14 -0500

    changed some things

--- Project1/Keyword.py
+++ Project1/Keyword.py
@@ -1,5 +1,4 @@
 import sys
-from keywordFunctions import*
 
 # pull filename from command line
 filename = sys.argv[1]
@@ -7,6 +6,21 @@ filename = sys.argv[1]
 # open an read the file that was passed in through command line
 f = open(filename, "r")
 text = f.read()
+
+def textToString(s):
+    words = []
+    current_word = ''
+    for c in s:
+        if c.isalpha() or c.isdigit() or c == '_':
+            current_word += c
+        else:
+            if current_word:
+                words.append(current_word)
+                current_word = ''
+    if current_word:
+        words.append(current_word)
+    return words
+
 
 words = textToString(text)  # Convert the text to a string
 
