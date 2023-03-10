b6cf8b11ef269d70d812bb5d5cf1b59cb78b2ca1
aydano <obrienaydan630@gmail.com>
2023-03-10 08:51:27 -0500

    initial commit

---
+++ Project1-Nick/Part3.py
@@ -0,0 +1,74 @@
+import sys
+
+# pull filename from command line
+filename = sys.argv[1]
+
+# open an read the file that was passed in through command line
+f = open(filename, "r")
+text = f.read()
+
+def textToString(s):
+    words = []
+    current_word = ''
+    for c in s:
+        if c.isalpha() or c.isdigit() or c == '_':
+            current_word += c
+        else:
+            if current_word:
+                words.append(current_word)
+                current_word = ''
+    if current_word:
+        words.append(current_word)
+    return words
+
+
+words = textToString(text)  # Convert the text to a string
+
+contexts = {}
+for i, word in enumerate(words):   # enumerate words so that we can loop through them
+    if i >= 2 and i <= len(words) - 3:
+        key = words[i].lower()
+        context = ' '.join(words[i-2:i+3]) # add the two words before and after the keyword
+        if key in contexts:
+            contexts[key].append(context)
+        else:
+            contexts[key] = [context]
+    elif i == 1:    # check for the second word
+        key = words[i].lower()
+        context = ' '.join(words[i-1:i+3])
+        if key in contexts:
+            contexts[key].append(context)
+        else:
+            contexts[key] = [context]
+    elif i == 0:    # check for the first word
+        key = words[i].lower()
+        context = ' '.join(words[:i+3])
+        if key in contexts:
+            contexts[key].append(context)
+        else:
+            contexts[key] = [context]
+    elif i == len(words)-2:     # check for the second to last word
+        key = words[i].lower()
+        context = ' '.join(words[i-2:i+2])
+        if key in contexts:
+            contexts[key].append(context)
+        else:
+            contexts[key] = [context]
+    elif i == len(words) - 1:   # check for the last word
+        key = words[i].lower()
+        context = ' '.join(words[i-2:])
+        if key in contexts:
+            contexts[key].append(context)
+        else:
+            contexts[key] = [context]
+
+
+while True:
+    keyword = input('Enter a keyword or enter "Q" to quit: ').lower()
+    if keyword == 'q':
+        break
+    if keyword in contexts:
+        for context in contexts[keyword]:
+            print(context)
+    else:
+        print('The keyword entered is not in the file.')
